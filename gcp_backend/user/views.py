8936dab6f2017eaf719b3626729f090fe50d73ae
Jatin <jatin.k0jatin@gmail.com>
2022-12-22 23:36:45 +0530

    New API added and documentation created
    Added all required API and updated README

--- gcp_backend/user/views.py
+++ gcp_backend/user/views.py
@@ -5,13 +5,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import re
+pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+s = r'(0|91)?[6-9][0-9]{9}'
+class Userinfo(APIView):
+    queryset= User.objects.all()
+    def get(self, request, user_id):
+        x = User.objects.values_list('name', 'email','ph_num', 'organization', 'role').filter(id = user_id)
+        x= x[0]
+        return Response({"name": x[0], "email": x[1], "ph_num":x[2], "organization": x[3], "role": x[4]},status=status.HTTP_200_OK)
 class UserCreation(APIView):
     queryset= User.objects.all()
     def post(self, request):
             serializer = UserSerializer(data=request.data)
             if serializer.is_valid():
-                pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-                s = r'(0|91)?[6-9][0-9]{9}'
+                
                 if(request.data['role']=="student" and Organization.objects.get(id=request.data['organization'])):
                     if re.match(pat,request.data['email']) and (True if request.data.get('ph_num', '') is '' else re.match(s, request.data['ph_num'])):
                             serializer.save()
@@ -36,13 +43,25 @@ class UserCreation(APIView):
         if not User_instance:
             return Response(
                 {"Message": "User with id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        if(request.data.get('id', '') is not '' or request.data.get('created_at', '') is not '' or request.data.get('updated_at', '') is not '' or request.data.get('is_active', '') is not '' or request.data.get('role', '') is not ''):
+            return Response({"status":"error", "Message":"You cannot edit id, created by, updated by, is active and role through api call"}, status=status.HTTP_409_CONFLICT)
+        if(request.data.get('email', '') is not '' and not re.match(pat,request.data['email'])):
+            return Response(
+                {"Message": "Email is not valid"}, 
+                status=status.HTTP_406_NOT_ACCEPTABLE
+            )
+        if(request.data.get('ph_num', '') is not '' and not re.match(s, request.data['ph_num'])):
+            return Response(
+                {"Message": "Phone number is not valid"}, 
+                status=status.HTTP_405_METHOD_NOT_ALLOWED
             )
         serializer = UserSerializer(instance=User_instance, data = request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(user_id, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status":"success","user id":user_id}, status=status.HTTP_200_OK)
+        return Response({"status":"success", "Message":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class OrganisationView(APIView):
     queryset = Organization.objects.all()
