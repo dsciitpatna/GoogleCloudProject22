0ee1a32068ceba98839187964b7b7b7a42e005c5
Jatin <jatin.k0jatin@gmail.com>
2022-12-24 23:15:29 +0530

    Improved Authentification.

--- gcp_backend/user/utility.py
+++ gcp_backend/user/utility.py
@@ -11,20 +11,21 @@ def autherize(func):
         try:
             cookie = args[1].COOKIES['jwt']
         except :
-            return Response({"message": "Cookie not found"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Cookie not found"}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
 
         try:
             payload = jwt.decode(cookie, COOKIE_ENCRYPTION_SECRET, algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
-            return Response( { 'message' : 'Cookie expired' }, status=status.HTTP_400_BAD_REQUEST)
+            return Response( { 'message' : 'Cookie expired' }, status=status.HTTP_408_REQUEST_TIMEOUT)
             
         user = User.objects.get(userid=payload['id'])
         if not user:
             return Response(
                 {"Message": "User with id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_204_NO_CONTENT
             )
         kwargs['user'] = user
+        kwargs['role'] = user.role
         return func(*args, **kwargs)
 
     return wrapper
