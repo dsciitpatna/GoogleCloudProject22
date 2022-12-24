85c65c232b1861f2e73be269a12c07fb196e592f
Shilpa <skcodes29@gmail.com>
2022-12-24 21:09:54 +0530

    Create Event Page Added

--- GCCP_frontend/gccp_frontend/src/App.js
+++ GCCP_frontend/gccp_frontend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
+import Login from './pages/Login'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
-        
+        <Login/>
       </header>
     </div>
   );
