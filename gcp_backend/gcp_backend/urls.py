55edfbf2d4959ff121ed88d52552c97346796cd7
Jatin <jatin.k0jatin@gmail.com>
2022-12-26 22:56:22 +0530

    Configured

--- gcp_backend/gcp_backend/urls.py
+++ gcp_backend/gcp_backend/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
+from user.urls import urlpatterns
+from events.urls import urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
     path('user/', include('user.urls')),
     path('jet/', include('jet.urls', 'jet')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
