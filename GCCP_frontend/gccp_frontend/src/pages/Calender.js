8aeed692c853376f35aa2a104eac0e2cd989bae0
aritroCoder <ogcodinggod@gmail.com>
2022-12-29 21:22:39 +0530

    added modal popup

--- GCCP_frontend/gccp_frontend/src/pages/Calender.js
+++ GCCP_frontend/gccp_frontend/src/pages/Calender.js
@@ -5,91 +5,28 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { INITIAL_EVENTS, createEventId } from "../config/events";
-import Sidebar from '../components/sidebar/index'
-import Header from '../components/header/index'
+import Sidebar from "../components/sidebar/index";
+import Header from "../components/header/index";
+import ModelComp from "../components/modelComp";
 
 import "../css/calender.css";
 
-export default class DemoApp extends React.Component {
-  state = {
-    weekendsVisible: true,
-    currentEvents: [],
+export default function DemoApp() {
+  // state = {
+  //   weekendsVisible: true,
+  //   currentEvents: [],
+  //   showModal: false,
+  // };
+
+  const [weekendsVisible, setWeekendsVisible] = React.useState(true);
+  const [currentEvents, setCurrentEvents] = React.useState([]);
+  const [showModal, setShowModal] = React.useState(false);
+
+  const handleWeekendsToggle = () => {
+    setWeekendsVisible(!weekendsVisible);
   };
 
-  render() {
-    return (
-      <div className="demo-app sidebar-mini skin-black-light">
-        <Header />
-      <Sidebar />
-      <div className="content-wrapper">
-        {/* {this.renderSidebar()} */}
-        <div className="demo-app-main content">
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={this.state.weekendsVisible}
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            select={this.handleDateSelect}
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
-            eventAdd={function(){}}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
-          />
-        </div>
-        </div>
-      </div>
-    );
-  }
-
-  renderSidebar() {
-    return (
-      <div className="demo-app-sidebar">
-        <div className="demo-app-sidebar-section">
-          <h2>Instructions</h2>
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-        <div className="demo-app-sidebar-section">
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.weekendsVisible}
-              onChange={this.handleWeekendsToggle}
-            ></input>
-            toggle weekends
-          </label>
-        </div>
-        <div className="demo-app-sidebar-section">
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>{this.state.currentEvents.map(renderSidebarEvent)}</ul>
-        </div>
-      </div>
-    );
-  }
-
-  handleWeekendsToggle = () => {
-    this.setState({
-      weekendsVisible: !this.state.weekendsVisible,
-    });
-  };
-
-  handleDateSelect = (selectInfo) => {
+  const handleDateSelect = (selectInfo) => {
     let title = prompt("Please enter a new title for your event");
     let calendarApi = selectInfo.view.calendar;
 
@@ -106,21 +43,83 @@ export default class DemoApp extends React.Component {
     }
   };
 
-  handleEventClick = (clickInfo) => {
-    if (
-      window.confirm(
-        `Are you sure you want to delete the event '${clickInfo.event.title}'`
-      )
-    ) {
-      clickInfo.event.remove();
-    }
+  const handleEventClick = (clickInfo) => {
+    setShowModal(true);
   };
 
-  handleEvents = (events) => {
-    this.setState({
-      currentEvents: events,
-    });
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
   };
+
+  return (
+    <div className="demo-app sidebar-mini skin-black-light">
+      <Header />
+      <Sidebar />
+      <div className="content-wrapper">
+        {/* {renderSidebar()} */}
+        <div className="demo-app-main content">
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={weekendsVisible}
+            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+            select={handleDateSelect}
+            eventContent={renderEventContent} // custom render function
+            eventClick={handleEventClick}
+            eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+            /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+          />
+          {showModal ? (
+            <div className="modal fade in" style={{ display: "block" }}>
+              <ModelComp closeHandler={setShowModal} />
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+
+  function renderSidebar() {
+    return (
+      <div className="demo-app-sidebar">
+        <div className="demo-app-sidebar-section">
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <label>
+            <input
+              type="checkbox"
+              checked={weekendsVisible}
+              onChange={handleWeekendsToggle}
+            ></input>
+            toggle weekends
+          </label>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 function renderEventContent(eventInfo) {
@@ -145,5 +144,4 @@ function renderSidebarEvent(event) {
       <i>{event.title}</i>
     </li>
   );
-};
-
+}
